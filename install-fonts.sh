93e496df239acaf537da10ff61e6a62dd52dc74d
Rune Juhl Jacobsen <rune@juhljacobsen.dk>
2016-06-22 13:21:32 +0200

    Added Ubuntu to install-fonts

--- install-fonts.sh
+++ install-fonts.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Quickie so I don't have to think next time I need to install a good font.
+set -e
 
 uid=$(id -u)
 
@@ -19,6 +20,18 @@ cd $io_tmp
 7z x $io_name
 cp ./*.ttf ~/.fonts
 [[ $uid == 0 ]] && sudo cp ./*.ttf /usr/local/share/fonts
+
+# Ubuntu
+ubuntu_url='http://font.ubuntu.com/'
+ubuntu_path=$(curl "${ubuntu_url}" | grep -Eo '../download/ubuntu-[a-z0-9.-]+')
+ubuntu_tmp=$(mktemp -d)
+ubuntu_name=$(basename "${ubuntu_path}")
+wget "${ubuntu_url}/${ubuntu_path}" -O "${ubuntu_tmp}/${ubuntu_name}"
+cd "$ubuntu_tmp"
+7z e "$ubuntu_name"
+cp ./*.ttf ~/.fonts
+[[ $uid == 0 ]] && sudo cp ./*.ttf /usr/local/share/fonts
+
 
 # rebuild font cache
 fc-cache -fv
