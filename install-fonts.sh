d4ae7d75aeaeda49094901d79d9862150052cba0
Rune Juhl Jacobsen <runejuhl@petardo.dk>
2018-05-03 13:44:32 +0200

    Fix bugs in install-fonts

--- install-fonts.sh
+++ install-fonts.sh
@@ -7,13 +7,13 @@ uid=$(id -u)
 # Terminus
 wget http://files.ax86.net/terminus-ttf/files/latest.zip -O /tmp/terminus.zip
 cd /tmp
-unzip terminus.zip
+unzip -o terminus.zip
 [[ $uid == 0 ]] && sudo cp terminus-ttf-*/TerminusTTF* /usr/local/share/fonts/
 mkdir -p ~/.fonts/
 cp terminus-ttf-*/TerminusTTF* ~/.fonts/
 
 # Iosevka
-IFS=' ' read io_url io_name <<< $(curl https://api.github.com/repos/be5invis/iosevka/releases/latest | jq -r '.assets[0].browser_download_url, .assets[0].name')
+IFS=' ' read io_url io_name <<< $(curl https://api.github.com/repos/be5invis/iosevka/releases/latest | jq -r '.assets[0].browser_download_url, .assets[0].name' | tr '\n' ' ')
 io_tmp=$(mktemp -d)
 wget "${io_url}" -O "${io_tmp}/${io_name}"
 cd $io_tmp
